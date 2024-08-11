6d1095eea5e68584fe94ebcaab7e6f31f3d2672d
Alex Straton <alexandrastraton1@gmail.com>
2024-08-11 19:05:19 +0100

    centered the container displaying the weather in the middle of the page

--- src/app/(pages)/weather/page.js
+++ src/app/(pages)/weather/page.js
@@ -21,33 +21,35 @@ export default function WeatherPage() {
   }
   //sunny icon, temp, add a way to switch between F and Celsius
   return (
-    <main
-      className='bg-sky-300 mt-52 rounded-xl p-6 max-w-lg content-center
+    <section className='flex items-center justify-center min-h-screen bg-gray-100'>
+      <main
+        className='bg-sky-300 mt-52 rounded-xl p-6 max-w-lg content-center
     '>
-      <p className='text-center text-4xl '>Here is today's weather</p>
-      <div className='text-center mt-20 text-2xl'>
-        {weatherData ? (
-          <div>
-            <p>Location: {weatherData.location.name}</p>
-            <p>Region: {weatherData.location.region}</p>
-            <p>Country: {weatherData.location.country}</p>
-            <p>Local Time: {weatherData.location.localtime}</p>
-            <p>{weatherData.current.condition.text}</p>
-            <img src={weatherData.current.condition.icon}></img>
-            <p> Day: {weatherData.current.is_day}</p>
-            <p>Wind: {weatherData.current.wind_kph} kph</p>
-            <p>Humidity: {weatherData.current.humidity}</p>
-            <p>UV: {weatherData.current.uv}</p>
-            <p>Precipitation: {weatherData.current.precip_mm}</p>
-            <p>Temp: {weatherData.current.temp_c} Celsius</p>
-            <p>Temp: {weatherData.current.temp_f} Celsius</p>
-          </div>
-        ) : (
-          <p>No weather data available.</p>
-        )}
-      </div>
+        <p className='text-center text-4xl '>Here is today's weather</p>
+        <div className='text-center mt-20 text-2xl'>
+          {weatherData ? (
+            <div>
+              <p>Location: {weatherData.location.name}</p>
+              <p>Region: {weatherData.location.region}</p>
+              <p>Country: {weatherData.location.country}</p>
+              <p>Local Time: {weatherData.location.localtime}</p>
+              <p>{weatherData.current.condition.text}</p>
+              <img src={weatherData.current.condition.icon}></img>
+              <p> Day: {weatherData.current.is_day}</p>
+              <p>Wind: {weatherData.current.wind_kph} kph</p>
+              <p>Humidity: {weatherData.current.humidity}</p>
+              <p>UV: {weatherData.current.uv}</p>
+              <p>Precipitation: {weatherData.current.precip_mm}</p>
+              <p>Temp: {weatherData.current.temp_c} Celsius</p>
+              <p>Temp: {weatherData.current.temp_f} Celsius</p>
+            </div>
+          ) : (
+            <p>No weather data available.</p>
+          )}
+        </div>
 
-      <button onClick={FetchWeather}>Fetch Weather</button>
-    </main>
+        <button onClick={FetchWeather}>Fetch Weather</button>
+      </main>
+    </section>
   );
 }
